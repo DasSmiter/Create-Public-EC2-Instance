af12864c471f3f1cc292e20b77570b598be56e43
DasSmiter <37093268+DasSmiter@users.noreply.github.com>
2021-10-08 17:29:37 -0500

    Updated outputs and commented.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,4 @@
 output "domain-name" {
+  #The only default output we're including is the DNS for our public EC2 instance so the user can verify if it deployed correctly.
   value = aws_instance.web.public_dns
-}
-
-output "application-url" {
-  value = "${aws_instance.web.public_dns}/index.php"
-}
-
-output "Instance-Subnet-ID" {
-  value = aws_security_group.web-sg.id
 }
