e86d5125aeabefb1184465dc531b6f162d0f8b7a
DasSmiter <37093268+DasSmiter@users.noreply.github.com>
2021-10-08 17:27:37 -0500

    Updated Terraform.tf to comment on providers we are using.

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,10 @@
 terraform {
   required_providers {
+    #Since we are setting up an EC2 instance we will be using AWS services for this.
     aws = {
       source = "hashicorp/aws"
     }
+    #This is a Terraform provider that we use to provide a random name for our components.
     random = {
       source = "hashicorp/random"
     }
